2409ab5a714138949bd82927ba643b7845ecb9c1
Maruf Khan <maruf1609@gmail.com>
2021-07-28 18:27:09 +0600

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:ecom/screens/signup.dart';
+void main() {
+  runApp(MaterialApp(
+    initialRoute: '/',
+    routes: {
+      '/':(context) => SignUp(),
+
+    },
+  ));
+}
+
---
+++ lib/screens/signup.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class SignUp extends StatelessWidget {
+  const SignUp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          child: Column(
+            children: [
+              Container(
+                height: 200,
+                color: Colors.blue,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
